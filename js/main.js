4e29e99a81b6f316fb4ea4942f2b07bb023c3ce5
Ian Vezzoso <ianvezzoso@gmail.com>
2023-02-15 16:57:15 -0300

    problema-con-bucle

---
+++ js/main.js
@@ -0,0 +1,25 @@
+function calcular_impuesto (precio){
+    let precioFinal = impuestos * precio
+    return console.log("El precio final del producto es: "+"$"+precioFinal)
+}
+
+let impuestos = 1.75
+let rta = "salir"
+
+while(rta=="salir"){
+    let precio = Number(prompt("Ingrese el precio del juego en AR$"))
+    if (precio <= 0 || isNaN(precio)){
+        console.log("Por favor ingrese un número valido")
+        
+    }    
+    else{
+        calcular_impuesto(precio)}
+    }
+
+rta =(prompt("Ingrese \"salir\" para finalizar"))
+
+
+
+
+
+
