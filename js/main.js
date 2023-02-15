d2e76683983489aadca0053924d2a86bd7137f0e
Ian Vezzoso <ianvezzoso@gmail.com>
2023-02-15 17:53:00 -0300

    funcional

--- js/main.js
+++ js/main.js
@@ -1,26 +1,21 @@
 function calcular_impuesto (precio){
     let precioFinal = impuestos * precio
-    return console.log("El precio final del producto es: "+"$"+precioFinal)
+    return console.log("El precio final del juego con un total de impuestos del 75% es: "+"$"+precioFinal+ " (AR$)")
 }
 
 let impuestos = 1.75
 let precio = 0
 let rta = ""
 
-/*
-let precio = (prompt("Ingrese el precio del juego en AR$"))
+while(rta == ""){
+    precio = (prompt("Ingrese el precio del juego en AR$"))
     if (precio <= 0 || isNaN(precio)){
         console.log("Por favor ingrese un número valido")
     }    
     else{
         calcular_impuesto(precio)
-}
-*/
-
-while(rta == ""){
-    precio = (prompt("Ingrese el precio del juego en AR$"))
-    calcular_impuesto(precio)
-    rta = prompt("Ingrese la palabra salir para finalizar o presione ENTER para continuar")
+    rta = prompt("Ingrese la palabra \"salir\" para finalizar o presione ENTER para continuar") .toLowerCase()
+    }
 }
 
 
