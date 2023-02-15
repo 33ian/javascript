736963cf343ca2ef8ed3487ca7a0416d87c29c7c
Ian Vezzoso <ianvezzoso@gmail.com>
2023-02-15 17:41:56 -0300

    bucle-simple-funcional

--- js/main.js
+++ js/main.js
@@ -4,16 +4,24 @@ function calcular_impuesto (precio){
 }
 
 let impuestos = 1.75
+let precio = 0
+let rta = ""
 
-let precio = Number(prompt("Ingrese el precio del juego en AR$"))
+/*
+let precio = (prompt("Ingrese el precio del juego en AR$"))
     if (precio <= 0 || isNaN(precio)){
         console.log("Por favor ingrese un número valido")
     }    
     else{
         calcular_impuesto(precio)
-    }
+}
+*/
 
-
+while(rta == ""){
+    precio = (prompt("Ingrese el precio del juego en AR$"))
+    calcular_impuesto(precio)
+    rta = prompt("Ingrese la palabra salir para finalizar o presione ENTER para continuar")
+}
 
 
 
