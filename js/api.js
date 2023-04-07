7fd63483eb4b1cb12abed559f6a023b394c66e38
Ian Vezzoso <ianvezzoso@gmail.com>
2023-04-06 21:38:09 -0300

    usuarios

--- js/api.js
+++ js/api.js
@@ -1,16 +1,16 @@
 const comentarios = document.getElementById("comentarios")
 
 
-fetch('https://jsonplaceholder.typicode.com/comments')
+fetch('https://jsonplaceholder.typicode.com/users')
 
     .then(response => response.json())
 
     .then(listaUsuarios => listaUsuarios.forEach(user => {
         comentarios.innerHTML += `
         
-            <p>ID del comentario: ${user.id}</p>
-            <p>Username: ${user.name}</p>
-            <p>${user.body}</p>
+            <p> Username: ${user.username}</p>
+            <p> Nombre: ${user.name}</p>
+            <p>ID de usuario: ${user.id}</p>
         
         `
     }))
